a7c7dd968b19e4093fbf253dd44d88b5ecede1f0
chetya17 <113277797+chetya17@users.noreply.github.com>
2023-03-13 12:35:34 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction import text
 from sklearn.metrics.pairwise import cosine_similarity
-data = pd.read_csv("Dataset upload karoo", encoding='latin1')
-data.head()
+data = pd.read_csv("C:\\Users\CHETAN\\Desktop\\google sdg challenge.csv", encoding='latin1')
 articles = data["Article"].tolist()
 uni_tfidf = text.TfidfVectorizer(input=articles, stop_words="english")
 uni_matrix = uni_tfidf.fit_transform(articles)
@@ -11,5 +10,9 @@ uni_sim = cosine_similarity(uni_matrix)
 def recommend_articles(x):
     return ", ".join(data["Title"].loc[x.argsort()[-5:-1]])
 data["Recommended Articles"] = [recommend_articles(x) for x in uni_sim]
-data.head()
-print(data["Recommended Articles"][22]) \
+array = data["Title"].to_numpy()
+name = input()
+for i in range(0, len(array)):
+    if name in array[i]:
+        print(data.loc[[i]])
+        break
